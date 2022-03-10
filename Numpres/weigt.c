d44ca5bd22d1a1b0edb875c2b891a7a6539fbbf8
LegendaryHog <kuteinikov.sd@phystech.edu>
2022-03-10 16:02:22 +0300

    weigt

---
+++ Numpres/weigt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*typedef struct weight {
+    int* right;
+    int  r_size;
+    int* left; 
+    int  l_size;
+} weigt;*/
+
+int pow_3 (int n)
+{
+    int res = 1;
+    for (int i = 0; i < n; i++)
+    {
+        res *= 3;
+    }
+    return res;
+}
+
+
+int main (void)
+{
+    scanf ("%d\n", N);
+    
+    return 0;
+}
